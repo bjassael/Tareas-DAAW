27a9d47b73fc82a9a67cb097918f522678978b2d
Benjamin assael <benjaminassael@protonmail.com>
2020-04-12 20:57:51 -0400

    .

--- tarea2/handleInput.js
+++ tarea2/handleInput.js
@@ -1,3 +1,38 @@
+// const handleKeyDown = (event) => {
+//   const keyName = event.key;
+//   if (keyName === "a") {
+//     document.getElementById("myBtnA").classList.add("active");
+//   }
+//   if (keyName === "s") {
+//     document.getElementById("myBtnS").classList.add("active");
+//   }
+//   if (keyName === "d") {
+//     document.getElementById("myBtnD").classList.add("active");
+//   }
+//   if (keyName === "f") {
+//     document.getElementById("myBtnF").classList.add("active");
+//   }
+// }
+
+// const handleKeyUp = (event) => {
+//   const keyName = event.key;
+
+//   if (keyName === "a") {
+//     document.getElementById("myBtnA").classList.remove("active");
+//   }
+//   if (keyName === "s") {
+//     document.getElementById("myBtnS").classList.remove("active");
+//   }
+//   if (keyName === "d") {
+//     document.getElementById("myBtnD").classList.remove("active");
+//   }
+//   if (keyName === "f") {
+//     document.getElementById("myBtnF").classList.remove("active");
+//   }
+
+// }
+
+
 const handleKeyPress = (event) => {
   const keyName = event.key;
 
@@ -15,5 +50,8 @@ const handleKeyPress = (event) => {
     }
   }
 };
+
+// document.addEventListener("keydown", handleKeyDown);
+// document.addEventListener("keyup", handleKeyUp);
 
 document.addEventListener("keypress", handleKeyPress);
