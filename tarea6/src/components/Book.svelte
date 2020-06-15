86f072794c760858a780a105803367878241d021
Javier Bahamondes <jfbahamondes@uc.cl>
2020-06-14 23:58:16 -0400

    added genred

--- tarea6/src/components/Book.svelte
+++ tarea6/src/components/Book.svelte
@@ -2,6 +2,12 @@
   import { createEventDispatcher } from "svelte";
   const dispatch = createEventDispatcher();
   export let bookinfo;
+  $: {
+    if (bookinfo.subject === undefined) {
+      bookinfo.subject = [];
+    }
+  }
+  console.log("bookinfo.subject", bookinfo.subject);
 </script>
 
 <style>
@@ -23,7 +29,10 @@
   class="book"
   on:click={() => dispatch('selectbook', {
       title: bookinfo.title_suggest,
-      author: bookinfo.author_name ? bookinfo.author_name[0] : 'no-author'
+      author: bookinfo.author_name ? bookinfo.author_name[0] : 'no-author',
+      genre: bookinfo.subject.slice(0, 3).join(' / ')
     })}>
-  {bookinfo.title_suggest} - {bookinfo.author_name ? bookinfo.author_name[0] : 'no-author'}
+  {bookinfo.title_suggest} - {bookinfo.author_name ? bookinfo.author_name[0] : 'no-author'}{bookinfo.subject.length > 0 ? ' - ' + bookinfo.subject
+        .slice(0, 3)
+        .join(' / ') : ''}
 </div>
