debaaf9b6a2ee604c9072756c167d3de34addf40
Javier Bahamondes <jfbahamondes@uc.cl>
2020-06-14 23:38:43 -0400

    working, on entering book name search on api for the author

---
+++ tarea6/src/components/Book.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
+  export let bookinfo;
+</script>
+
+<style>
+  .book {
+    padding: 10px;
+    background-color: rgba(62, 120, 153, 0.5);
+    color: white;
+    margin: 20px 0;
+    border-radius: 10px;
+    font-size: 20px;
+  }
+  .book:hover {
+    cursor: pointer;
+    background-color: rgba(62, 120, 153, 1);
+  }
+</style>
+
+<div
+  class="book"
+  on:click={() => dispatch('selectbook', {
+      title: bookinfo.title_suggest,
+      author: bookinfo.author_name ? bookinfo.author_name[0] : 'no-author'
+    })}>
+  {bookinfo.title_suggest} - {bookinfo.author_name ? bookinfo.author_name[0] : 'no-author'}
+</div>
